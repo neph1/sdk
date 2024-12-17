9f1cdedaaf0aa4a966c717e6bfd5055b07e1ff3a
rickard <rickard@mindemia.com>
2024-12-17 19:43:12 +0100

    add templates for MonkeyZone and JaimesAscent

---
+++ jme3-templates/src/com/jme3/gde/templates/monkeyzone/MonkeyZoneDownloadPanel.java
@@ -0,0 +1,189 @@
+/*
+ * Copyright (c) 2024 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jme3.gde.templates.monkeyzone;
+
+import java.awt.Component;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import org.openide.WizardDescriptor;
+import org.openide.WizardValidationException;
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle;
+
+/**
+ * Panel just asking for basic info.
+ */
+@SuppressWarnings({"unchecked", "rawtypes"})
+public class MonkeyZoneDownloadPanel implements WizardDescriptor.Panel,
+        WizardDescriptor.ValidatingPanel, WizardDescriptor.FinishablePanel {
+
+    private WizardDescriptor wizardDescriptor;
+    private MonkeyZoneDownloadPanelVisual component;
+    
+    static String ZIP_NAME = "MonkeyZone.zip";
+    static String DOWNLOAD_FOLDER = System.getProperty("java.io.tmpdir");
+
+    public MonkeyZoneDownloadPanel() {
+    }
+    
+    public int doDownloadZip() {
+        return downloadFile("https://github.com/jMonkeyEngine/monkeyzone/archive/refs/heads/master.zip", DOWNLOAD_FOLDER, ZIP_NAME);
+    }
+    
+    private int downloadFile(String fileURL, String saveDir, String fileName) {
+        HttpURLConnection httpConn = null;
+        BufferedInputStream inputStream = null;
+        FileOutputStream fileOutputStream = null;
+        
+        final File outputFile = new File(saveDir, fileName);
+        
+        if (outputFile.exists()) {
+            return 1;
+        }
+
+        try {
+            // Create URL object
+            URL url = new URL(fileURL);
+            httpConn = (HttpURLConnection) url.openConnection();
+
+            // Check HTTP response code
+            int responseCode = httpConn.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Open input stream from the HTTP connection
+                inputStream = new BufferedInputStream(httpConn.getInputStream());
+                
+                
+
+                // Create output stream to save the file
+                fileOutputStream = new FileOutputStream(outputFile);
+
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    fileOutputStream.write(buffer, 0, bytesRead);
+                }
+
+                return 1;
+            }
+        } catch (IOException e) {
+            return 0;
+        } finally {
+            // Close resources
+            try {
+                if (inputStream != null) inputStream.close();
+                if (fileOutputStream != null) fileOutputStream.close();
+                if (httpConn != null) httpConn.disconnect();
+            } catch (IOException ex) {
+                return 0;
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public Component getComponent() {
+        if (component == null) {
+            component = new MonkeyZoneDownloadPanelVisual(this);
+            component.setName(NbBundle.getMessage(MonkeyZoneDownloadPanel.class, "LBL_DownloadProjectStep"));
+        }
+        return component;
+    }
+
+    @Override
+    public HelpCtx getHelp() {
+        return new HelpCtx("sdk.download_project");
+    }
+
+    @Override
+    public boolean isValid() {
+        getComponent();
+        return component.valid(wizardDescriptor);
+    }
+    
+    private final Set<ChangeListener> listeners = new HashSet<>(1); // or can use ChangeSupport in NB 6.0
+
+    @Override
+    public final void addChangeListener(ChangeListener l) {
+        synchronized (listeners) {
+            listeners.add(l);
+        }
+    }
+
+    @Override
+    public final void removeChangeListener(ChangeListener l) {
+        synchronized (listeners) {
+            listeners.remove(l);
+        }
+    }
+
+    protected final void fireChangeEvent() {
+        Set<ChangeListener> ls;
+        synchronized (listeners) {
+            ls = new HashSet<>(listeners);
+        }
+        ChangeEvent ev = new ChangeEvent(this);
+        for (ChangeListener l : ls) {
+            l.stateChanged(ev);
+        }
+    }
+
+    @Override
+    public void readSettings(Object settings) {
+        wizardDescriptor = (WizardDescriptor) settings;
+        component.read(wizardDescriptor);
+    }
+
+    @Override
+    public void storeSettings(Object settings) {
+        WizardDescriptor d = (WizardDescriptor) settings;
+        component.store(d);
+    }
+
+    @Override
+    public boolean isFinishPanel() {
+        return false;
+    }
+
+    @Override
+    public void validate() throws WizardValidationException {
+        getComponent();
+        component.validate(wizardDescriptor);
+    }
+}
---
+++ jme3-templates/src/com/jme3/gde/templates/monkeyzone/build.gradle.ftl
@@ -0,0 +1,65 @@
+plugins {
+    id 'java'
+    id 'application'
+}
+
+group 'com.monkeyzone'
+version '1.0'
+
+mainClassName = "com.monkeyzone.MonkeyZone"
+
+repositories {
+    mavenCentral()
+}
+
+project.ext {
+  jmeVer = '3.7.0-stable'
+}
+
+project(":assets") {
+    apply plugin: "java"
+
+    buildDir = rootProject.file("build/assets")
+
+    sourceSets {
+        main {
+            resources {
+                srcDir '.'
+            }
+        }
+    }
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+dependencies {
+
+  implementation "org.jmonkeyengine:jme3-core:$jmeVer"
+  implementation "org.jmonkeyengine:jme3-desktop:$jmeVer"
+  implementation "org.jmonkeyengine:jme3-lwjgl:$jmeVer"
+  implementation "org.jmonkeyengine:jme3-lwjgl:$jmeVer"
+  implementation "com.github.stephengold:Heart:9.0.0"
+  implementation "com.github.stephengold:Minie:8.0.0"
+  implementation project("assets")
+
+}
+
+jar {
+    manifest {
+        attributes 'Main-Class': "$mainClassName"
+    }
+}
+
+wrapper {
+    gradleVersion = '8.6'
+}
